eb4a6a1904cacb3a5ab7cd21fc00fd254471615f
Mrozowski, Kornel <kpmrozowski@gmail.com>
2021-06-12 23:19:10 +0200

    works till second :D

--- include/Utils/rgb2hsv.h
+++ include/Utils/rgb2hsv.h
@@ -18,54 +18,20 @@ int high_S = max_value_S * 1.00;
 int low_S = max_value_S * 0.08;
 int high_V = max_value_V * 1.00;
 int low_V = max_value_V * 0.00;
-const String window_capture_name = "Video Capture";
-const String window_detection_name = "Object Detection";
-static void on_low_H_thresh_trackbar(int, void *)
-{
-    low_H = min(high_H-1, low_H);
-    setTrackbarPos("Low H", window_detection_name, low_H);
-}
-static void on_high_H_thresh_trackbar(int, void *)
-{
-    high_H = max(high_H, low_H+1);
-    setTrackbarPos("High H", window_detection_name, high_H);
-}
-static void on_low_S_thresh_trackbar(int, void *)
-{
-    low_S = min(high_S-1, low_S);
-    setTrackbarPos("Low S", window_detection_name, low_S);
-}
-static void on_high_S_thresh_trackbar(int, void *)
-{
-    high_S = max(high_S, low_S+1);
-    setTrackbarPos("High S", window_detection_name, high_S);
-}
-static void on_low_V_thresh_trackbar(int, void *)
-{
-    low_V = min(high_V-1, low_V);
-    setTrackbarPos("Low V", window_detection_name, low_V);
-}
-static void on_high_V_thresh_trackbar(int, void *)
-{
-    high_V = max(high_V, low_V+1);
-    setTrackbarPos("High V", window_detection_name, high_V);
-}
 
 // color_tresholder(Mat img_in, Mat img_out)
-Mat color_tresholder(Mat img_in) {
-//    createTrackbar("Low H", window_detection_name, &low_H, max_value_H, on_low_H_thresh_trackbar);
-//    createTrackbar("High H", window_detection_name, &high_H, max_value_H, on_high_H_thresh_trackbar);
-//    createTrackbar("Low S", window_detection_name, &low_S, max_value_S, on_low_S_thresh_trackbar);
-//    createTrackbar("High S", window_detection_name, &high_S, max_value_S, on_high_S_thresh_trackbar);
-//    createTrackbar("Low V", window_detection_name, &low_V, max_value_V, on_low_V_thresh_trackbar);
-//    createTrackbar("High V", window_detection_name, &high_V, max_value_V, on_high_V_thresh_trackbar);
+Mat color_tresholder(const Mat img_in) {
 
    Mat frame_HSV, frame_BGR, frame_threshold, output;
    // Convert from BGR to HSV colorspace
    cvtColor(img_in, frame_HSV, COLOR_BGR2HSV);
    // Detect the object based on HSV Range Values
    inRange(frame_HSV, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
+   // imshow("frame_threshold", frame_threshold);
+   // waitKey();
    bitwise_and(img_in, img_in, output, frame_threshold);
+   // imshow("output", output);
+   // waitKey();
    return output;
 }
 }
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -11,97 +11,101 @@
 
 using namespace twm;
 
-void display(std::string wndName, cv::Mat& img) {
+cv::Mat copyOneImage(const cv::Mat &origImage) {
+    cv::Mat image;
+    origImage.copyTo(image);
+    return image;
+}
+
+void display(std::string wndName, const cv::Mat& img) {
     int frame_width = img.cols;
 	int frame_height = img.rows;
-    float scale = 1080. / frame_height;
-    cv::resize(img, img, cv::Size(static_cast<std::size_t>(frame_width * scale), static_cast<std::size_t>(frame_height * scale)));
-    imshow(wndName, img);
+    // float scale = 1080. / frame_height;
+    float scale = 2000. / frame_height;
+    cv::Mat img_copy = copyOneImage(img);
+    cv::resize(img_copy, img_copy, cv::Size(static_cast<std::size_t>(frame_width * scale), static_cast<std::size_t>(frame_height * scale)));
+    imshow(wndName, img_copy);
     cv::waitKey();
 }
+
+
 // cv::rectangle(img, cv::Point(NW.x, NW.y), cv::Point(SE.x, SE.y), cv::Scalar(B,G,R), 1);
 
 int main() {
     srand(time(NULL));
     int thresh = 50, N = 11;
-    const char* wndname = "Square Detection Demo";
-    cv::Mat img_orig = cv::imread( cv::samples::findFile( "../../../../images/game1/20210612_113122.jpg" ), cv::IMREAD_COLOR );
-    cv::Mat img_orig2 = cv::imread( cv::samples::findFile( "../../../../images/game1/20210612_113205.jpg" ), cv::IMREAD_COLOR );
-    
-    auto mask = utils::color_tresholder(img_orig);
-    for (size_t x = 0; x < img_orig2.rows; x++) {
-        for (size_t y = 0; y < img_orig2.cols; y++) {
-            cv::Vec3b pixel = img_orig2.at<cv::Vec3b>(x,y);
-            cv::Vec3b mask_pixel = mask.at<cv::Vec3b>(x,y);
-            if (mask_pixel[0] != 0 || mask_pixel[1] != 0 || mask_pixel[2] != 0) {
-                pixel[2] = 194; // V
-                pixel[1] = 194;  // S
-                pixel[0] = 194; // H
-                img_orig2.at<cv::Vec3b>(x,y) = pixel;
+    cv::Mat img_first = cv::imread( cv::samples::findFile( "../../../../images/game1/1.jpg" ), cv::IMREAD_COLOR );
+    // Inicjalizacja petli
+    int frame_width = img_first.cols;
+	int frame_height = img_first.rows;
+    cv::Mat mask(frame_height, frame_width, CV_8UC3, cv::Scalar(0, 0, 0));
+    cv::Mat image_orig_masked_old;
+    using carcassonne::TilePlacement;
+    mb::vector2d<TilePlacement> m_board;
+
+    for( size_t img_id = 1; img_id <= 71; img_id++) {
+        // std::cout << name << '\n' ;
+        std::string path = "/media/kmro/WW OD 47-03-21/ADATA/game1";
+        std::string full_path = path + std::to_string(img_id) + std::string(".jpg");
+        cv::Mat img_orig = cv::imread( cv::samples::findFile( full_path.c_str() ), cv::IMREAD_COLOR );
+        cv::Mat img_orig_masked = copyOneImage(img_orig);
+        for (size_t x = 0; x < img_orig.rows; x++) {
+            for (size_t y = 0; y < img_orig.cols; y++) {
+                cv::Vec3b pixel = img_orig_masked.at<cv::Vec3b>(x,y);
+                cv::Vec3b mask_pixel = mask.at<cv::Vec3b>(x,y);
+                if (mask_pixel[0] != 0 || mask_pixel[1] != 0 || mask_pixel[2] != 0) {
+                    pixel[2] = 194; // V
+                    pixel[1] = 194;  // S
+                    pixel[0] = 194; // H
+                    img_orig_masked.at<cv::Vec3b>(x,y) = pixel;
+                }
             }
         }
-    }
-    // if (true) {
-        for( const auto& name : twm::hough::get_filenames("../../../../images/game1/") ) {
-            std::cout << name << '\n' ;
-            cv::Mat img_orig = cv::imread( cv::samples::findFile( name.c_str() ), cv::IMREAD_COLOR );
+        // display("img_orig_masked", img_orig_masked);
+        image_orig_masked_old = copyOneImage(img_orig_masked);
 
-            cv::Mat img;
-            cv::GaussianBlur(img_orig, img, cv::Size(5, 5), 0);
-            std::vector<std::vector<cv::Point>> foundSquares;
-
-            twm::hough::findSquares(img_orig2, foundSquares);
-            std::vector<Square> squares;
-            for (auto square : foundSquares) {
-                std::cout << square[0] << ", " << square[1] << ", " << square[2] << ", " << square[3] << ", " << std::endl;
-                Square s = Square(square);
-                squares.push_back(s);
-                s.print();
-                s.draw(img, 255, 255, 0);
-            }
-            polylines(img_orig2, foundSquares, true, cv::Scalar(0, 255, 0), 3, cv::LINE_AA);
-            std::cout << "squares.size() = " << squares.size() << std::endl;
-            display(wndname, img_orig2);
-            std::vector<cv::Mat> squareImages = twm::hough::getSquareImages(img_orig, squares);
-            std::cout << "squareImages.size() = " << squareImages.size() << std::endl;
-            twm::colors::Classifier classifier;
-            cv::Mat detected_square;
-
-            for (const auto &im : squareImages) {
-                detected_square = im; // zrobic lepiej, teraz zaklada ze jest jeden kwadrat
-                // cv::imshow("a", im);
-                //cv::waitKey();
-            }
-
-            std::pair<std::string, int> detected_tile_info = classifier.classifyHog(detected_square);
-            std::cout << "Znaleziony obrazek: " << detected_tile_info.first << " " << detected_tile_info.second <<  std::endl;
-
-            using carcassonne::TilePlacement;
-            mb::vector2d<TilePlacement> m_board;
-            std::uint8_t t = 0, rotation = 0;
-            m_board.set(70, 70, TilePlacement{.type = t, .rotation = rotation});
+        // detekcja prostokatow
+        cv::Mat img_blured;
+        cv::GaussianBlur(image_orig_masked_old, img_blured, cv::Size(5, 5), 0);
+        std::vector<std::vector<cv::Point>> foundSquares;
+        twm::hough::findSquares(img_blured, foundSquares);
+        std::vector<Square> squares;
+        cv::Mat img_orig_polylines = copyOneImage(img_orig);
+        for (auto square : foundSquares) { // wiadomo ze bedzie tylko 1 square
+            std::cout << square[0] << ", " << square[1] << ", " << square[2] << ", " << square[3] << ", " << std::endl;
+            Square s = Square(square);
+            squares.push_back(s);
+            s.print();
+            // s.draw(img_orig_polylines, 255, 255, 0);
         }
+        polylines(img_orig_polylines, foundSquares, true, cv::Scalar(0, 255, 0), 3, cv::LINE_AA);
+        std::cout << "squares.size() = " << squares.size() << std::endl;
+        const char* wndname = "Square Detection Demo";
+        display(wndname, img_orig_polylines);
 
+        // wycinanie wykrytego prostokata
+        std::vector<cv::Mat> squareImages = twm::hough::getSquareImages(img_orig, squares);
+        std::cout << "squareImages.size() = " << squareImages.size() << std::endl;
+
+        // klasyfikacja
+        twm::colors::Classifier classifier;
+        cv::Mat detected_square;
+        for (const auto &im : squareImages) { // wiadomo ze bedzie tylko 1 square
+            detected_square = im;
+        }
+        std::pair<std::string, int> detected_tile_info = classifier.classifyHog(detected_square);
+        std::cout << "Znaleziony obrazek: " << detected_tile_info.first << " " << detected_tile_info.second <<  std::endl;
         
+        // dodaj kafelek do wirtualnej planszy
+        std::uint8_t t = 0, rotation = 0;
+        m_board.set(70, 70, TilePlacement{.type = t, .rotation = rotation});
 
-    // } else {
-    //     for( const auto& name : twm::hough::get_filenames("sequential") ) {
-    //         std::cout << name << '\n' ;
-    //         const char * str = name.c_str();
-    //         cv::Mat img = twm::hough::detect_liness(str);
-
-    //         /* Saving images, to the resources folder */
-    //         if (false) {
-    //             std::string saving_path("./output_images/");
-    //             saving_path.append(name);
-    //             std::cout << saving_path << std::endl;
-    //             cv::imwrite(saving_path, img);
-    //         }
-    //         // break;
-    //         int c = cv::waitKey();
-    //         if( c ==  27 )
-    //             break;
-    //     }
-    // }
+        // display("MASK INPUT", img_orig);
+        cv::Mat mask1 = utils::color_tresholder(image_orig_masked_old);
+        mask = copyOneImage(mask1);
+        // cv::imshow("MASK OUTPUT", mask);
+        cv::waitKey();
+    }
+   
     return 0;
 }
