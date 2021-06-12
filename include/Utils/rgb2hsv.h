dba37be78c0293086f660741439ae8032a5ed273
Mrozowski, Kornel <kpmrozowski@gmail.com>
2021-06-12 20:24:54 +0200

    Mask works

--- include/Utils/rgb2hsv.h
+++ include/Utils/rgb2hsv.h
@@ -13,7 +13,7 @@ const int max_value_S = 255;
 const int max_value_V = 255;
 
 int high_H = max_value_H * 0.928;
-int low_H = max_value_H * 0.246;
+int low_H = max_value_H * 0;//0.246;
 int high_S = max_value_S * 1.00;
 int low_S = max_value_S * 0.08;
 int high_V = max_value_V * 1.00;
@@ -53,27 +53,20 @@ static void on_high_V_thresh_trackbar(int, void *)
 
 // color_tresholder(Mat img_in, Mat img_out)
 Mat color_tresholder(Mat img_in) {
-   createTrackbar("Low H", window_detection_name, &low_H, max_value_H, on_low_H_thresh_trackbar);
-   createTrackbar("High H", window_detection_name, &high_H, max_value_H, on_high_H_thresh_trackbar);
-   createTrackbar("Low S", window_detection_name, &low_S, max_value_S, on_low_S_thresh_trackbar);
-   createTrackbar("High S", window_detection_name, &high_S, max_value_S, on_high_S_thresh_trackbar);
-   createTrackbar("Low V", window_detection_name, &low_V, max_value_V, on_low_V_thresh_trackbar);
-   createTrackbar("High V", window_detection_name, &high_V, max_value_V, on_high_V_thresh_trackbar);
+//    createTrackbar("Low H", window_detection_name, &low_H, max_value_H, on_low_H_thresh_trackbar);
+//    createTrackbar("High H", window_detection_name, &high_H, max_value_H, on_high_H_thresh_trackbar);
+//    createTrackbar("Low S", window_detection_name, &low_S, max_value_S, on_low_S_thresh_trackbar);
+//    createTrackbar("High S", window_detection_name, &high_S, max_value_S, on_high_S_thresh_trackbar);
+//    createTrackbar("Low V", window_detection_name, &low_V, max_value_V, on_low_V_thresh_trackbar);
+//    createTrackbar("High V", window_detection_name, &high_V, max_value_V, on_high_V_thresh_trackbar);
 
    Mat frame_HSV, frame_BGR, frame_threshold, output;
    // Convert from BGR to HSV colorspace
    cvtColor(img_in, frame_HSV, COLOR_BGR2HSV);
    // Detect the object based on HSV Range Values
    inRange(frame_HSV, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
-   bitwise_and(img_in, img_in, frame_threshold);
-    
-    cv::imshow("frame_threshold", frame_threshold);
-    cv::waitKey();
-    cv::imshow("img_in", img_in);
-    cv::waitKey();
-   // Convert from BGR to HSV colorspace
-   // cvtColor(frame_threshold, frame_BGR, COLOR_HSV2BGR);
-   return img_in;
+   bitwise_and(img_in, img_in, output, frame_threshold);
+   return output;
 }
 }
 
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -19,30 +19,32 @@ int main() {
     cv::Mat img_orig = cv::imread( cv::samples::findFile( "../../../../images/game1/20210612_113122.jpg" ), cv::IMREAD_COLOR );
     // cv::GaussianBlur(img_orig, img_orig, cv::Size(9, 9), 0);
     cv::Mat img_orig2 = cv::imread( cv::samples::findFile( "../../../../images/game1/20210612_113205.jpg" ), cv::IMREAD_COLOR );
-
-    auto frame_threshold = utils::color_tresholder(img_orig);
+    // cv::GaussianBlur(img_orig2, img_orig2, cv::Size(9, 9), 0);
+    
+    auto mask = utils::color_tresholder(img_orig);
     // Show the frames
-    cv::imshow(wndname, frame_threshold);
+    cv::imshow("mask", mask);
+    cv::waitKey();
+    for (size_t x = 0; x < img_orig2.rows; x++) {
+        for (size_t y = 0; y < img_orig2.cols; y++) {
+            cv::Vec3b pixel = img_orig2.at<cv::Vec3b>(x,y);
+            cv::Vec3b mask_pixel = mask.at<cv::Vec3b>(x,y);
+            // fmt::print("{} ", static_cast<int>(mask_pixel[0]));
+            if (mask_pixel[0] != 0 || mask_pixel[1] != 0 || mask_pixel[2] != 0) {
+                pixel[2] = 194; // V
+                pixel[1] = 194;  // S
+                pixel[0] = 194; // H
+                img_orig2.at<cv::Vec3b>(x,y) = pixel;
+            }
+        }
+    }
+    cv::imshow("img_orig2", img_orig2);
     cv::waitKey();
 
-    // cv::GaussianBlur(img_orig2, img_orig2, cv::Size(9, 9), 0);
     cv::Mat img_diff;
     cv::absdiff(img_orig2, img_orig, img_diff);
     cv::threshold(img_diff, img_diff, 80, 255, cv::THRESH_BINARY);
     cv::erode(img_diff, img_diff, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3)));
-    
-    cv::imshow("img_diff", img_diff);
-    cv::waitKey();
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -52,22 +54,21 @@ int main() {
 
 
         cv::Mat img_hsv, img_new;
+
         cv::cvtColor(img_orig, img_hsv, cv::COLOR_BGR2HSV);
-        unsigned int maxH = 0.928 * 179;
-        unsigned int minH = 0.246 * 179;
+        unsigned int minH = 0.00 * 179;
+        unsigned int maxH = 0.9 * 179;
+        unsigned int minS = 0.04 * 255;
         unsigned int maxS = 1 * 255;
-        unsigned int minS = 0.08 * 255;
         for (size_t x = 0; x < img_orig.rows; x++) {
             for (size_t y = 0; y < img_orig.cols; y++) {
                 cv::Vec3b pixel = img_hsv.at<cv::Vec3b>(x,y);
-                if (pixel[0] > maxH || pixel[0] < minH || pixel[1] < minS || pixel[1] > maxS) {
-                    pixel[0] = 0;
-                    pixel[1] = 0;
-                    pixel[2] = 0;
+                if (pixel[0] < minH || pixel[0] > maxH || pixel[1] < minS || pixel[1] > maxS) {
+                    pixel[2] = 0; // V
+                    pixel[1] = 0;  // S
+                    pixel[0] = 179; // H
                     img_hsv.at<cv::Vec3b>(x,y) = pixel;
-                    // std::cout << "PixelH: " << static_cast<int>(pixel[0]) << std::endl;
                 }
-                // std::cout << "PixelH: " << static_cast<int>(pixel[0]) << std::endl;
             }
         }
         cv::cvtColor(img_hsv, img_new, cv::COLOR_HSV2BGR);
@@ -90,7 +91,9 @@ int main() {
         // cv::Mat img = img_orig;
         std::vector<std::vector<cv::Point>> foundSquares;
         // twm::hough::findSquares(img, foundSquares);
-        twm::hough::findSquares(img_diff, foundSquares);
+
+
+        twm::hough::findSquares(img_new, foundSquares);
         std::vector<Square> squares;
         for (auto square : foundSquares) {
             std::cout << square[0] << ", " << square[1] << ", " << square[2] << ", " << square[3] << ", " << std::endl;
