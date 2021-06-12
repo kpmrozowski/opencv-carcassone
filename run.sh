0076e1cf804b2b584b9a7c04ab59fdbd43b5595e
Akoszewski <akoszews@gmail.com>
2021-06-12 16:16:36 +0200

    Naprawiony run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 rm ./output_images/resources/*
 CXX=/usr/bin/g++-10 CC=/usr/bin/gcc-10 cmake -S . -B out/build_terminal
-CXX=/usr/bin/g++-10 CC=/usr/bin/gcc-10 make -C out/build_terminal -j12 && out/build_terminal/src/main/twm
+CXX=/usr/bin/g++-10 CC=/usr/bin/gcc-10 make -C out/build_terminal -j12 && cd out/build_terminal/src/main && ./twm && cd -
 # CXX=/usr/bin/g++-10 CC=/usr/bin/gcc-10 make -C out/build_terminal -j12 && out/build_terminal/src/Hough/twm_homography
