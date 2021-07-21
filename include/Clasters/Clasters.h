50f7e3b5872f51647f4db0c209aa95fa3cb5a0d2
Kornel <kpmrozowski@gmail.com>
2021-07-21 22:25:11 +0200

    added matplotlib-cpp

--- include/Clasters/Clasters.h
+++ include/Clasters/Clasters.h
@@ -3,16 +3,18 @@
 
 #include <Hough/Line.h>
 #include <Hough/Point.h>
+#include <Utils/matplotlibcpp.h>
 
 #include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <limits>
 #include <vector>
+#include <numeric>
 #include <fmt/core.h>
 // #include "Square.h"
 #define _USE_MATH_DEFINES
-using PointLines = std::vector<std::tuple<Line, Point<double>, Point<double>>>;
+using PointLines = std::vector<std::pair<Point<double>, Point<double>>>;
 
 class LinesClasters {
  public:
@@ -25,11 +27,49 @@ class LinesClasters {
   void print() {
       fmt::print("fi1\t fi2\n");
     for (const auto& pl : m_pointLines) {
-      fmt::print("{}\t {}\n", std::get<1>(pl).fi, std::get<2>(pl).fi);
+      fmt::print("{}\t {}\n", pl.first.fi, pl.second.fi);
+    }
+  }
+
+  template <typename T, typename Compare>
+  std::vector<std::size_t> find_permutation(const std::vector<T>& vec, const Compare& compare) {
+    std::vector<std::size_t> p(vec.size());
+    std::iota(p.begin(), p.end(), 0);
+    std::sort(p.begin(), p.end(), [&](std::size_t i, std::size_t j) {
+      return compare(vec[i], vec[j]);
+    });
+    return p;
+  }
+
+  template <typename T>
+  std::vector<T> apply_permutation(const std::vector<T>& vec, const std::vector<std::size_t>& p) {
+    std::vector<T> sorted_vec(vec.size());
+    std::transform(p.begin(), p.end(), sorted_vec.begin(),
+                   [&](std::size_t i) { return vec[i]; });
+    return sorted_vec;
+  }
+
+  template <typename T>
+  void apply_permutation_in_place(std::vector<T>& vec, const std::vector<std::size_t>& p) {
+    std::vector<bool> done(vec.size());
+    for (std::size_t i = 0; i < vec.size(); ++i) {
+      if (done[i]) {
+        continue;
+      }
+      done[i] = true;
+      std::size_t prev_j = i;
+      std::size_t j = p[i];
+      while (i != j) {
+        std::swap(vec[prev_j], vec[j]);
+        done[j] = true;
+        prev_j = j;
+        j = p[j];
+      }
     }
   }
 
   void twolinesClaster(std::vector<Line> linesvec, int img_rows, int img_cols) {
+    auto size = linesvec.size();
     // circle coeffictients
     const double x_o = img_rows / 2.;
     const double y_o = img_cols / 2.;
@@ -84,8 +124,63 @@ class LinesClasters {
         fmt::print("{}: r_o <= d = {}\n", i, d_0);
         continue;
       }
-      m_pointLines.emplace_back(*it, Point(ax, ay, aro, afi), Point(bx, by, bro, bfi));
+      m_pointLines.emplace_back(Point(ax, ay, aro, afi), Point(bx, by, bro, bfi));
     }
+    std::vector<std::vector<double>> distances_table{size, std::vector<double>(size, -1.)};
+    for (int i = 0; i < size; ++i) {
+      for (int j = 0; j < size; ++j) {
+        distances_table.at(i).at(j) = sqrt(
+            pow(m_pointLines.at(i).first.fi - m_pointLines.at(j).first.fi, 2) +
+            pow(m_pointLines.at(i).second.fi - m_pointLines.at(j).second.fi,
+                2));
+      }
+    }
+    auto sorted_distances_table{distances_table};
+    std::vector<std::vector<long unsigned int>> permutations_table{size, std::vector<long unsigned int>(size, -1)};
+    std::vector<double> means(size, -1.);
+    std::vector<double> medians(size, -1.);
+    for (int i = 0; i < size; ++i) {// std::vector<double>& distances : sorted_distances_table) {
+      auto distances{sorted_distances_table.at(i)};
+      auto p = find_permutation(
+          distances,
+          [](double a, double b) {
+            return a < b;
+          });
+      permutations_table.at(i) = p;
+      means.at(i) = std::accumulate(distances.begin(), distances.end(), 0.) / static_cast<double>(size);
+      medians.at(i) = distances.at(size / 2);
+      apply_permutation_in_place(sorted_distances_table.at(i), p);
+    }
+    auto p_means = find_permutation(
+          means,
+          [](double a, double b) {
+            return a < b;
+          });
+    auto p_medians = find_permutation(
+          medians,
+          [](double a, double b) {
+            return a < b;
+          });
+    apply_permutation_in_place(means, p_means);
+    apply_permutation_in_place(medians, p_medians);
+
+    // vectorA = apply_permutation(vectorA, p);
+    // vectorB = apply_permutation(vectorB, p);
+    std::vector<double> x(size, -1), y(size, -1), x1(size, -1), y1(size, -1), x2(size, -1), y2(size, -1);
+    for (int i = 0; i < size; ++i) {
+      x1.at(i) = m_pointLines.at(i).first.x;
+      y1.at(i) = m_pointLines.at(i).first.y;
+      x2.at(i) = m_pointLines.at(i).second.x;
+      y2.at(i) = m_pointLines.at(i).second.y;
+      x.at(i) = m_pointLines.at(i).first.fi;
+      y.at(i) = m_pointLines.at(i).second.fi;
+    }
+    // matplotlibcpp::plot(x1, y1, "r.", x2, y2, "b.");
+    // matplotlibcpp::show();
+    matplotlibcpp::plot(x, y, "r.");
+    matplotlibcpp::plot(y, x, "b.");
+    matplotlibcpp::show();
+
     this->print();
     double zz = 1.;
   }
--- src/main/tests.cpp
+++ src/main/tests.cpp
@@ -7,18 +7,45 @@
 #include <Utils/display.h>
 #include <Utils/rgb2hsv.h>
 #include <fmt/core.h>
+#include <Utils/matplotlibcpp.h>
 
 #include <iostream>
 #include <limits>
+// #include <cmath>
 
-using namespace twm;
+namespace plt = matplotlibcpp;
 
-int main() {
-  auto p1{Point(0., 1.0000000000000)};
-  auto p2{Point(0., 1.)};
-  if (p1 == p2)
-    fmt::print("Equal\n");
-  else
-    fmt::print("Unequal\n");
-  LinesClasters linesClasters;
+int main()
+{
+    const int n = 20;
+    std::vector<std::vector<double>> matrix;
+
+    for (int i = 0; i < n; ++i) {
+        std::vector<double> row;
+        for (int j = 0; j < n; ++j) {
+            if (i == j)
+                row.push_back(-2);
+            else if (j == i - 1 || j == i + 1)
+                row.push_back(1);
+            else
+                row.push_back(0);
+        }
+        matrix.push_back(row);
+    }
+
+    plt::spy(matrix, 0.5, {{"marker", "o"}});
+    plt::show();
+
+    return 0;
 }
+
+// using namespace twm;
+// int main() {
+//   auto p1{Point(0., 1.0000000000000)};
+//   auto p2{Point(0., 1.)};
+//   if (p1 == p2)
+//     fmt::print("Equal\n");
+//   else
+//     fmt::print("Unequal\n");
+//   // LinesClasters linesClasters;
+// }
